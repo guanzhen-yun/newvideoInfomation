19514d3653f2cd900d55f3459818bdc4f3b00a75
guanz1 <guanz1@ziroom.com>
2020-05-02 22:23:57 +0800

    ndk开发代码提交

--- app/src/main/java/com/ziroom/newvideoinfomation/main/shenzhen/ShenZhenFragment.java
+++ app/src/main/java/com/ziroom/newvideoinfomation/main/shenzhen/ShenZhenFragment.java
@@ -1,6 +1,7 @@
 package com.ziroom.newvideoinfomation.main.shenzhen;
 
 import android.content.Context;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -8,10 +9,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.z.opengl.OpenGlManager;
 import com.z.videoinfomationndk.MainActivity;
 import com.ziroom.newvideoinfomation.R;
 import com.ziroom.newvideoinfomation.base.BaseFragment;
 import com.ziroom.newvideoinfomation.base.ViewInject;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
 import butterknife.BindView;
 
@@ -29,10 +34,32 @@ public class ShenZhenFragment extends BaseFragment {
 
     @BindView(R.id.tv_position)
     TextView tvPosition;
+    @BindView(R.id.gl_surface_view)
+    GLSurfaceView glSurfaceView;
 
     @Override
     public void afterBindView() {
         tvPosition.setText(MainActivity.stringFromJNI());
+        glSurfaceView.setRenderer(new GLSurfaceView.Renderer() {
+            @Override
+            public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+                //为缓冲区 设置清除颜色的值 相当于初始化
+                OpenGlManager.onSurfaceCreated();
+            }
+
+            @Override
+            public void onSurfaceChanged(GL10 gl10, int width, int height) {
+                //设置 视图 大小
+                OpenGlManager.onSurfaceChanged(width, height);
+            }
+
+            //绘制的时候 每一帧 都会被系统调用 在Android中 默认最高绘制效率 为1秒 60帧
+            @Override
+            public void onDrawFrame(GL10 gl10) {
+                //设置 色彩
+                OpenGlManager.onDrawFrame();
+            }
+        });
     }
 
     @Override
